577e6ab929cea2b6db3242b77e447f60b7ffaeab
Juaka <juakacc@gmail.com>
2020-04-22 13:28:40 -0300

    Documentando recurso motorista

--- src/controller/motoristas.js
+++ src/controller/motoristas.js
@@ -1,17 +1,248 @@
-const express = require('express')
-const check_auth = require('../middleware/check_auth')
-const motoristas = require('../service/motoristas')
-const constantes = require('../constantes')
-const router = express.Router()
+const express = require("express");
+const check_auth = require("../middleware/check_auth");
+const motoristas = require("../service/motoristas");
+const constantes = require("../constantes");
+const router = express.Router();
 
-router.get('/', (req, res, next) => check_auth(req, res, next, constantes.MOTORISTA), motoristas.get_all)
+/**
+ * @apiDefine Header
+ * @apiHeaderExample {json} Header-Example:
+ *     {
+ *       "Authorization": "Bearer TOKEN"
+ *     }
+ */
 
-router.get('/:motoristaId', (req, res, next) => check_auth(req, res, next, constantes.MOTORISTA), motoristas.get_by_id)
+/**
+ * @api {get} /motoristas Retorna uma lista com os Motoristas
+ * @apiName getMotoristas
+ * @apiGroup Motoristas
+ *
+ * @apiUse Header
+ *
+ * @apiSuccess {Object[]} mostorista           Motoristas cadastrados.
+ * @apiSuccess {Number}   motorista.id         ID do motorista.
+ * @apiSuccess {String}   motorista.nome       Nome do motorista.
+ * @apiSuccess {String}   motorista.apelido    Nome do motorista.
+ * @apiSuccess {String}   motorista.cnh        Número da CNH do motorista.
+ * @apiSuccess {String{..2}}   motorista.categoria  Categoria da CNH do motorista.
+ * @apiSuccess {String}   motorista.telefone   Telefone do motorista.
+ * @apiSuccess {Boolean}     motorista.disponivel True caso o motorista esteja disponível, False caso contrário.
+ *
+ * @apiSuccessExample Success-Response:
+ *    HTTP/1.1 200 OK
+ *    [{
+ *      "id": 1,
+ *      "nome": "João",
+ *      "apelido": "joao",
+ *      "cnh": "12345",
+ *      "categoria": "AB",
+ *      "telefone": "999999999",
+ *      "disponivel": true
+ *    }, {
+ *      "id": 2,
+ *      "nome": "Maria",
+ *      "apelido": "maria",
+ *      "cnh": "54321",
+ *      "categoria": "B",
+ *      "telefone": "999999999",
+ *      "disponivel": false
+ *    }]
+ */
+router.get(
+  "/",
+  (req, res, next) => check_auth(req, res, next, constantes.MOTORISTA),
+  motoristas.get_all
+);
 
-router.post('/', (req, res, next) => check_auth(req, res, next, constantes.ADMIN), motoristas.salvar)
+/**
+ * @api {get} /motoristas/:motoristaId Retorna um Motorista específico
+ * @apiName getMotorista
+ * @apiGroup Motoristas
+ *
+ * @apiParam  {Number} motoristaId Id do motorista
+ *
+ * @apiUse Header
+ *
+ * @apiSuccess {Number}   id         ID do motorista.
+ * @apiSuccess {String}   nome       Nome do motorista.
+ * @apiSuccess {String}   apelido    Nome do motorista.
+ * @apiSuccess {String}   cnh        Número da CNH do motorista.
+ * @apiSuccess {String{..2}}   categoria  Categoria da CNH do motorista.
+ * @apiSuccess {String}   telefone   Telefone do motorista.
+ * @apiSuccess {Boolean}     disponivel True caso o motorista esteja disponível, False caso contrário.
+ * @apiSuccess {String[]} permissoes Permissões do motorista.
+ *
+ * @apiSuccessExample Success-Response:
+ *    HTTP/1.1 200 OK
+ *    {
+ *      "id": 1,
+ *      "nome": "João",
+ *      "apelido": "joao",
+ *      "cnh": "12345",
+ *      "categoria": "AB",
+ *      "telefone": "999999999",
+ *      "disponivel": true,
+ *      "permissoes": [
+ *        "motorista",
+ *        "admin"
+ *      ]
+ *    }
+ *
+ * @apiError MotoristaNotFound O <code>id</code> do motorista não foi encontrado.
+ *
+ * @apiErrorExample Error-Response:
+ *    HTTP/1.1 404 Not Found
+ *    {
+ *      "mensagem": "Motorista não encontrado",
+ *    }
+ */
+router.get(
+  "/:motoristaId",
+  (req, res, next) => check_auth(req, res, next, constantes.MOTORISTA),
+  motoristas.get_by_id
+);
 
-router.put('/:motoristaId', (req, res, next) => check_auth(req, res, next, constantes.MOTORISTA), motoristas.editar)
+/**
+ * @api {post} /motoristas Cadastra um Motorista
+ * @apiName saveMotorista
+ * @apiGroup Motoristas
+ *
+ * @apiUse Header
+ *
+ * @apiParam {String}  nome                    Nome do motorista.
+ * @apiParam {String}  apelido                 Apelido do motorista.
+ * @apiParam {String}  cnh                     Número da CNH do motorista.
+ * @apiParam {String{..2}}  categoria               Categoria da CNH do motorista.
+ * @apiParam {String}  telefone                Telefone do motorista.
+ * @apiParam {String}  senha                   Senha do motorista.
+ * @apiParam {Object}  [permissoes="{motorista: true}"]            Permissões do motorista
+ * @apiParam {Boolean} [permissoes[motorista]] Permissão de motorista
+ * @apiParam {Boolean} [permissoes[admin]]     Permissão de administrador
+ *
+ * @apiParamExample {json} Request-Example:
+ *     {
+ *       "nome": "João Costa",
+ *       "apelido": "joao123",
+ *       "cnh": "12345",
+ *       "categoria": "AC",
+ *       "telefone": "999999999",
+ *       "senha": "acasdoasdnk",
+ *       "permissoes": {
+ *          "motorista": true,
+ *          "admin": true,
+ *       }
+ *     }
+ *
+ * @apiSuccess {Number} id ID do motorista.
+ * @apiSuccess {String} nome  Nome do motorista.
+ * @apiSuccess {String} apelido  Nome do motorista.
+ * @apiSuccess {String} cnh  Número da CNH do motorista.
+ * @apiSuccess {String{..2}} categoria  Categoria da CNH do motorista.
+ * @apiSuccess {String} telefone  Telefone do motorista.
+ * @apiSuccess {Boolean} disponivel  True caso o motorista esteja disponível, False caso contrário.
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *    HTTP/1.1 201 Created
+ *    {
+ *      "id": 1,
+ *      "nome": "João",
+ *      "apelido": "joao",
+ *      "cnh": "12345",
+ *      "categoria": "AB",
+ *      "telefone": "999999999",
+ *      "disponivel": true
+ *    }
+ *
+ * @apiErrorExample Error-Response:
+ *    HTTP/1.1 400 Bad Request
+ *    {
+ *      "mensagem": "Parâmetro inválido",
+ *    }
+ */
+router.post(
+  "/",
+  (req, res, next) => check_auth(req, res, next, constantes.ADMIN),
+  motoristas.salvar
+);
 
-router.delete('/:motoristaId', (req, res, next) => check_auth(req, res, next, constantes.ADMIN), motoristas.deletar)
+/**
+ * @api {put} /motoristas/:motoristaId Atualiza um Motorista.
+ * @apiName updateMotorista
+ * @apiGroup Motoristas
+ *
+ * @apiUse Header
+ *
+ * @apiParam  {Number} motoristaId Id do motorista
+ *
+ * @apiParam {String}  [nome]                    Nome do motorista.
+ * @apiParam {String}  [apelido]                 Apelido do motorista.
+ * @apiParam {String}  [cnh]                     Número da CNH do motorista.
+ * @apiParam {String{..2}}  [categoria]               Categoria da CNH do motorista.
+ * @apiParam {String}  [telefone]                Telefone do motorista.
+ *
+ * @apiParamExample {json} Request-Example:
+ *     {
+ *       "nome": "João Costa",
+ *       "apelido": "joao123",
+ *       "cnh": "12345",
+ *       "categoria": "AC",
+ *       "telefone": "999999999"
+ *     }
+ *
+ * @apiSuccess {Number} id ID do motorista.
+ * @apiSuccess {String} nome  Nome do motorista.
+ * @apiSuccess {String} apelido  Nome do motorista.
+ * @apiSuccess {String} cnh  Número da CNH do motorista.
+ * @apiSuccess {String{..2}} categoria  Categoria da CNH do motorista.
+ * @apiSuccess {String} telefone  Telefone do motorista.
+ * @apiSuccess {Boolean} disponivel  True caso o motorista esteja disponível, False caso contrário.
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *    HTTP/1.1 200 OK
+ *    {
+ *      "id": 1,
+ *      "nome": "João",
+ *      "apelido": "joao",
+ *      "cnh": "12345",
+ *      "categoria": "AB",
+ *      "telefone": "999999999",
+ *      "disponivel": true
+ *    }
+ *
+ * @apiErrorExample Error-Response:
+ *    HTTP/1.1 400 Bad Request
+ *    {
+ *      "mensagem": "Motorista não encontrado",
+ *    }
+ *
+ * @apiErrorExample Error-Response:
+ *    HTTP/1.1 400 Bad Request
+ *    {
+ *      "mensagem": "Parâmetro inválido",
+ *    }
+ */
+router.put(
+  "/:motoristaId",
+  (req, res, next) => check_auth(req, res, next, constantes.MOTORISTA),
+  motoristas.editar
+);
 
-module.exports = router
+/**
+ * @api {delete} /motoristas/:motoristaId Deleta um Motorista.
+ * @apiName deleteMotorista
+ * @apiGroup Motoristas
+ *
+ * @apiParam  {Number} motoristaId Id do motorista
+ *
+ * @apiUse Header
+ *
+ * @apiSuccessExample Success-Response:
+ *    HTTP/1.1 204 No Content
+ */
+router.delete(
+  "/:motoristaId",
+  (req, res, next) => check_auth(req, res, next, constantes.ADMIN),
+  motoristas.deletar
+);
+
+module.exports = router;
--- src/service/motoristas.js
+++ src/service/motoristas.js
@@ -1,318 +1,331 @@
-const Motorista = require('../models').motorista
-const Permissao = require('../models').auth_permissoes
-const HttpStatus = require('http-status-codes')
-const bcrypt = require('bcryptjs')
-const constantes = require('../constantes')
+const Motorista = require("../models").motorista;
+const Permissao = require("../models").auth_permissoes;
+const HttpStatus = require("http-status-codes");
+const bcrypt = require("bcryptjs");
+const constantes = require("../constantes");
 
 exports.get_all = (req, res, next) => {
-    Motorista.findAll()
+  Motorista.findAll()
     .then(motoristas => {
-        const result = motoristas.map(motorista => {
-            delete motorista.dataValues.senha
-            return motorista
-        })
-        res.status(HttpStatus.OK).json(result)
+      const result = motoristas.map(motorista => {
+        delete motorista.dataValues.senha;
+        return motorista;
+      });
+      res.status(HttpStatus.OK).json(result);
     })
     .catch(err => {
-        console.log(err)
-        res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
-            mensagem: 'Erro interno no servidor'
-        })
-    })
-}
+      console.log(err);
+      res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        mensagem: "Erro interno no servidor",
+      });
+    });
+};
 
 exports.get_by_id = (req, res, next) => {
-    const id = req.params.motoristaId
+  const id = req.params.motoristaId;
 
-    Motorista.findByPk(id)
+  Motorista.findByPk(id)
     .then(motorista => {
-        if (!motorista) {
-            res.status(HttpStatus.NOT_FOUND).json({
-                mensagem: 'Motorista não encontrado'
-            })
-        } else {
-            Permissao.findAll({
-                where: { id_motorista: id }
-            })
-            .then(per => {
-                const permissoes = per.map(item => {
-                    return item.dataValues.permissao
-                })
-                motorista.dataValues.permissoes = permissoes.length > 0 ? permissoes : [constantes.MOTORISTA]
-                delete motorista.dataValues.senha
-                res.status(HttpStatus.OK).json(motorista.dataValues)
-            })
-            .catch(err => {
-                console.log(err)
-                delete motorista.dataValues.senha
-                res.status(HttpStatus.OK).json(motorista.dataValues)
-            })            
-        }
+      if (!motorista) {
+        res.status(HttpStatus.NOT_FOUND).json({
+          mensagem: "Motorista não encontrado",
+        });
+      } else {
+        Permissao.findAll({
+          where: {id_motorista: id},
+        })
+          .then(per => {
+            const permissoes = per.map(item => {
+              return item.dataValues.permissao;
+            });
+            motorista.dataValues.permissoes =
+              permissoes.length > 0 ? permissoes : [constantes.MOTORISTA];
+            delete motorista.dataValues.senha;
+            res.status(HttpStatus.OK).json(motorista.dataValues);
+          })
+          .catch(err => {
+            console.log(err);
+            delete motorista.dataValues.senha;
+            res.status(HttpStatus.OK).json(motorista.dataValues);
+          });
+      }
     })
     .catch(err => {
-        console.log(err)
-        res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
-            mensagem: 'Erro interno no servidor'
-        })
-    })
-}
+      console.log(err);
+      res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        mensagem: "Erro interno no servidor",
+      });
+    });
+};
 
 exports.salvar = async (req, res, next) => {
-    const { nome, apelido, cnh, categoria, telefone, senha, permissoes } = req.body
-    
-    const salvar = {
-        nome, apelido, cnh, categoria, telefone, senha, permissoes
-    }
+  const {nome, apelido, cnh, categoria, telefone, senha, permissoes} = req.body;
 
-    if (nome) {
-        if ('' === nome.trim())
-            return res.status(HttpStatus.BAD_REQUEST).json({
-                mensagem: 'Nome inválido'
-            })
-    } else {
-        return res.status(HttpStatus.BAD_REQUEST).json({
-            mensagem: 'O nome é obrigatório'
-        })
-    }
+  const salvar = {
+    nome,
+    apelido,
+    cnh,
+    categoria,
+    telefone,
+    senha,
+    permissoes,
+  };
 
-    if (apelido) {
-        if ('' === apelido.trim()) {
-            return res.status(HttpStatus.BAD_REQUEST).json({
-                mensagem: 'Apelido inválido'
-            })
-        } else {
-            const aux = await Motorista.findAll({
-                where: { apelido }
-            })
-            if (aux.length > 0) {
-                return res.status(HttpStatus.BAD_REQUEST).json({
-                    mensagem: 'O apelido já está em uso'
-                })
-            }
-            salvar.apelido = apelido.toLowerCase()
-        }
-    } else {
-        return res.status(HttpStatus.BAD_REQUEST).json({
-            mensagem: 'O apelido é obrigatório'
-        })
-    }
-    
-    if (senha) {
-        if ('' === senha.trim()) {
-            return res.status(HttpStatus.BAD_REQUEST).json({
-                mensagem: 'Senha inválida'
-            })
-        } else {
-            const salt = bcrypt.genSaltSync(10)
-            const senhaEnc = bcrypt.hashSync(senha, salt)
-            salvar.senha = senhaEnc
-        }
-    } else {
-        return res.status(HttpStatus.BAD_REQUEST).json({
-            mensagem: 'A senha é obrigatória'
-        })
-    }
+  if (nome) {
+    if ("" === nome.trim())
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        mensagem: "Nome inválido",
+      });
+  } else {
+    return res.status(HttpStatus.BAD_REQUEST).json({
+      mensagem: "O nome é obrigatório",
+    });
+  }
 
-    if (telefone) {
-        if ('' === telefone.trim())
-            return res.status(HttpStatus.BAD_REQUEST).json({
-                mensagem: 'Telefone inválido'
-            })
+  if (apelido) {
+    if ("" === apelido.trim()) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        mensagem: "Apelido inválido",
+      });
     } else {
+      const aux = await Motorista.findAll({
+        where: {apelido},
+      });
+      if (aux.length > 0) {
         return res.status(HttpStatus.BAD_REQUEST).json({
-            mensagem: 'O telefone é obrigatório'
-        })
+          mensagem: "O apelido já está em uso",
+        });
+      }
+      salvar.apelido = apelido.toLowerCase();
     }
+  } else {
+    return res.status(HttpStatus.BAD_REQUEST).json({
+      mensagem: "O apelido é obrigatório",
+    });
+  }
 
-    if (categoria) {
-        const cat = categoria.toUpperCase()
-        if (!['A', 'B', 'C', 'D', 'E', 'AB', 'AC', 'AD', 'AE'].includes(cat)) {
-            return res.status(HttpStatus.BAD_REQUEST).json({
-                mensagem: 'Categoria da CNH inválida. Valores aceitos: [A, B, C, D, E, AB, AC, AD, AE]'
-            })
-        }
-        salvar.categoria = cat
+  if (senha) {
+    if ("" === senha.trim()) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        mensagem: "Senha inválida",
+      });
     } else {
-        return res.status(HttpStatus.BAD_REQUEST).json({
-            mensagem: 'A categoria da CNH é obrigatória'
-        })
+      const salt = bcrypt.genSaltSync(10);
+      const senhaEnc = bcrypt.hashSync(senha, salt);
+      salvar.senha = senhaEnc;
     }
+  } else {
+    return res.status(HttpStatus.BAD_REQUEST).json({
+      mensagem: "A senha é obrigatória",
+    });
+  }
 
-    if (cnh) {
-        if ('' === cnh.trim()) {
-            return res.status(HttpStatus.BAD_REQUEST).json({
-                mensagem: 'Número da CNH inválido'
-            })
-        } else {
-            const aux = await Motorista.findAll({
-                where: { cnh }
-            })
-            if (aux.length > 0) {
-                return res.status(HttpStatus.BAD_REQUEST).json({
-                    mensagem: 'A CNH informada já está cadastrada'
-                })
-            }
-        }
+  if (telefone) {
+    if ("" === telefone.trim())
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        mensagem: "Telefone inválido",
+      });
+  } else {
+    return res.status(HttpStatus.BAD_REQUEST).json({
+      mensagem: "O telefone é obrigatório",
+    });
+  }
+
+  if (categoria) {
+    const cat = categoria.toUpperCase();
+    if (!["A", "B", "C", "D", "E", "AB", "AC", "AD", "AE"].includes(cat)) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        mensagem:
+          "Categoria da CNH inválida. Valores aceitos: [A, B, C, D, E, AB, AC, AD, AE]",
+      });
+    }
+    salvar.categoria = cat;
+  } else {
+    return res.status(HttpStatus.BAD_REQUEST).json({
+      mensagem: "A categoria da CNH é obrigatória",
+    });
+  }
+
+  if (cnh) {
+    if ("" === cnh.trim()) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        mensagem: "Número da CNH inválido",
+      });
     } else {
+      const aux = await Motorista.findAll({
+        where: {cnh},
+      });
+      if (aux.length > 0) {
         return res.status(HttpStatus.BAD_REQUEST).json({
-            mensagem: 'O número da CNH é obrigatório'
-        })
-    } 
+          mensagem: "A CNH informada já está cadastrada",
+        });
+      }
+    }
+  } else {
+    return res.status(HttpStatus.BAD_REQUEST).json({
+      mensagem: "O número da CNH é obrigatório",
+    });
+  }
 
-    Motorista.create(salvar)
+  Motorista.create(salvar)
     .then(motorista => {
-        delete motorista.dataValues.senha
+      delete motorista.dataValues.senha;
 
-        Permissao.create({  // padrao
-            id_motorista: motorista.id,
-            permissao: constantes.MOTORISTA
-        })
-        if (permissoes && permissoes.admin) {
-            Permissao.create({
-                id_motorista: motorista.id,
-                permissao: constantes.ADMIN
-            })
-        }
-        return res.status(HttpStatus.CREATED).json(motorista.dataValues)    
+      Permissao.create({
+        // padrao
+        id_motorista: motorista.id,
+        permissao: constantes.MOTORISTA,
+      });
+      if (permissoes && permissoes.admin) {
+        Permissao.create({
+          id_motorista: motorista.id,
+          permissao: constantes.ADMIN,
+        });
+      }
+      return res.status(HttpStatus.CREATED).json(motorista.dataValues);
     })
     .catch(err => {
-        console.log(err)
-        return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
-            mensagem: 'Erro interno do servidor'
-        })
-    })
-}
+      console.log(err);
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        mensagem: "Erro interno do servidor",
+      });
+    });
+};
 
 exports.editar = async (req, res, next) => {
-    const id = req.params.motoristaId
-    const { nome, apelido, cnh, categoria, telefone } = req.body
+  const id = req.params.motoristaId;
+  const {nome, apelido, cnh, categoria, telefone} = req.body;
 
-    const motoristaBD = await Motorista.findByPk(id)
-    if (!motoristaBD) {
-        return res.status(HttpStatus.BAD_REQUEST).json({
-            mensagem: 'Motorista não encontrado'
-        })
-    }
+  const motoristaBD = await Motorista.findByPk(id);
+  if (!motoristaBD) {
+    return res.status(HttpStatus.BAD_REQUEST).json({
+      mensagem: "Motorista não encontrado",
+    });
+  }
 
-    if (!req.userData.roles.includes(constantes.ADMIN) && req.userData.id != id) {
-        return res.status(HttpStatus.UNAUTHORIZED).json({
-            mensagem: 'Você não tem autorização para alterar esse motorista!'
-        })
-    }
+  if (!req.userData.roles.includes(constantes.ADMIN) && req.userData.id != id) {
+    return res.status(HttpStatus.UNAUTHORIZED).json({
+      mensagem: "Você não tem autorização para alterar esse motorista!",
+    });
+  }
 
-    const salvar = {
-        nome, apelido, cnh, categoria, telefone
-    }
+  const salvar = {
+    nome,
+    apelido,
+    cnh,
+    categoria,
+    telefone,
+  };
 
-    if (nome) {
-        if ('' === nome.trim())
-            return res.status(HttpStatus.BAD_REQUEST).json({
-                mensagem: 'Nome inválido'
-            })
+  if (nome) {
+    if ("" === nome.trim())
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        mensagem: "Nome inválido",
+      });
+  } else {
+    delete salvar.nome;
+  }
+
+  if (apelido) {
+    if ("" === apelido.trim()) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        mensagem: "Apelido inválido",
+      });
     } else {
-        delete salvar.nome
+      const motorista = await Motorista.findAll({
+        where: {apelido},
+      });
+      if (motorista.length > 0) {
+        if (motorista[0].id != id) {
+          return res.status(HttpStatus.BAD_REQUEST).json({
+            mensagem: "O apelido já está em uso",
+          });
+        }
+      }
+      salvar.apelido = apelido.toLowerCase();
     }
-    
+  } else {
+    delete salvar.apelido;
+  }
 
-    if (apelido) {
-        if ('' === apelido.trim()) {
-            return res.status(HttpStatus.BAD_REQUEST).json({
-                mensagem: 'Apelido inválido'
-            })
-        } else {
-            const motorista = await Motorista.findAll({
-                where: { apelido }
-            })
-            if (motorista.length > 0) {
-                if (motorista[0].id != id) {
-                    return res.status(HttpStatus.BAD_REQUEST).json({
-                        mensagem: 'O apelido já está em uso'
-                    })
-                }
-            }
-            salvar.apelido = apelido.toLowerCase()
-        }        
+  if (telefone) {
+    if ("" === telefone.trim())
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        mensagem: "Telefone inválido",
+      });
+  } else {
+    delete salvar.telefone;
+  }
+
+  if (categoria) {
+    const cat = categoria.toUpperCase();
+    if (!["A", "B", "C", "D", "E", "AB", "AC", "AD", "AE"].includes(cat))
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        mensagem:
+          "Categoria da CNH inválida. Valores aceitos: [A, B, C, D, E, AB, AC, AD, AE]",
+      });
+    salvar.categoria = cat;
+  } else {
+    delete salvar.categoria;
+  }
+
+  if (cnh) {
+    if ("" === cnh.trim()) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        mensagem: "Número da CNH inválido",
+      });
     } else {
-        delete salvar.apelido
+      const motorista = await Motorista.findAll({
+        where: {cnh},
+      });
+      if (motorista.length > 0) {
+        if (motorista[0].id != id) {
+          return res.status(HttpStatus.BAD_REQUEST).json({
+            mensagem: "A CNH informada já está em uso",
+          });
+        }
+      }
     }
-    
-    if (telefone) {
-        if ('' === telefone.trim())
-            return res.status(HttpStatus.BAD_REQUEST).json({
-                mensagem: 'Telefone inválido'
-            })
-    } else {
-        delete salvar.telefone
-    }    
+  } else {
+    delete salvar.cnh;
+  }
 
-    if (categoria) {
-        const cat = categoria.toUpperCase()
-        if (!['A', 'B', 'C', 'D', 'E', 'AB', 'AC', 'AD', 'AE'].includes(cat))
-            return res.status(HttpStatus.BAD_REQUEST).json({
-                mensagem: 'Categoria da CNH inválida. Valores aceitos: [A, B, C, D, E, AB, AC, AD, AE]'
-            })
-        salvar.categoria = cat
-    } else {
-        delete salvar.categoria
-    }
-
-    if (cnh) {
-        if ('' === cnh.trim()) {
-            return res.status(HttpStatus.BAD_REQUEST).json({
-                mensagem: 'Número da CNH inválido'
-            })
-        } else {
-            const motorista = await Motorista.findAll({
-                where: { cnh }
-            })
-            if (motorista.length > 0) {
-                if (motorista[0].id != id) {
-                    return res.status(HttpStatus.BAD_REQUEST).json({
-                        mensagem: 'A CNH informada já está em uso'
-                    })
-                }
-            }
-        }        
-    } else {
-        delete salvar.cnh
-    }
-
-    Motorista.update(salvar, {
-        where: { id }
-    })
+  Motorista.update(salvar, {
+    where: {id},
+  })
     .then(() => {
-        Motorista.findByPk(id)
+      Motorista.findByPk(id)
         .then(motorista2 => {
-            delete motorista2.dataValues.senha
-            return res.status(HttpStatus.OK).json(motorista2.dataValues)
+          delete motorista2.dataValues.senha;
+          return res.status(HttpStatus.OK).json(motorista2.dataValues);
         })
         .catch(err => {
-            console.log(err)
-            return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
-                mensagem: 'Erro interno no servidor'
-            })
-        })            
+          console.log(err);
+          return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+            mensagem: "Erro interno no servidor",
+          });
+        });
     })
     .catch(err => {
-        console.log(err)
-        return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
-            mensagem: 'Erro interno do servidor'
-        })
-    })
-}
+      console.log(err);
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        mensagem: "Erro interno do servidor",
+      });
+    });
+};
 
 exports.deletar = (req, res, next) => {
-    const id = req.params.motoristaId
+  const id = req.params.motoristaId;
 
-    Motorista.destroy({
-        where: { id }
-    })
+  Motorista.destroy({
+    where: {id},
+  })
     .then(() => {
-        res.status(HttpStatus.NO_CONTENT).send()
+      res.status(HttpStatus.NO_CONTENT).send();
     })
     .catch(err => {
-        res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
-            mensagem: err
-        })
-    })
-}
+      res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        mensagem: err,
+      });
+    });
+};
