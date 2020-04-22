caa84c0feb9a6d3db619d9ea6d3cf5b12c1a51df
Juaka <juakacc@gmail.com>
2020-04-22 09:16:40 -0300

    Validaçao do login

--- src/controller/login.js
+++ src/controller/login.js
@@ -1,53 +1,68 @@
-const express = require('express')
-const HttpStatus = require('http-status-codes')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-require('dotenv').config()
+const express = require("express");
+const HttpStatus = require("http-status-codes");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-const Motorista = require('../models').motorista
+const Motorista = require("../models").motorista;
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/', (req, res, next) => {
+router.post("/", (req, res, next) => {
+  const {apelido, senha} = req.body;
 
-    const { apelido, senha } = req.body
+  if (!apelido) {
+    return res.status(HttpStatus.BAD_REQUEST).json({
+      mensagem: "Apelido é obrigatório",
+    });
+  }
 
-    Motorista.findAll({
-        where: { apelido }
-    })
+  if (!senha) {
+    return res.status(HttpStatus.BAD_REQUEST).json({
+      mensagem: "Senha é obrigatória",
+    });
+  }
+
+  Motorista.findAll({
+    where: {apelido},
+  })
     .then(motoristas => {
-        if (motoristas.length < 1) {
-            return res.status(HttpStatus.BAD_REQUEST).json({
-                mensagem: 'Apelido inválido'
-            })
-        } 
-        const motorista = motoristas[0].dataValues
+      if (motoristas.length < 1) {
+        return res.status(HttpStatus.BAD_REQUEST).json({
+          mensagem: "Apelido inválido",
+        });
+      }
+      const motorista = motoristas[0].dataValues;
 
-        if (!bcrypt.compareSync(senha, motorista.senha)) {
-            return res.status(HttpStatus.BAD_REQUEST).json({
-                mensagem: 'Login e senha não correspondem'
-            })
+      if (!bcrypt.compareSync(senha, motorista.senha)) {
+        return res.status(HttpStatus.BAD_REQUEST).json({
+          mensagem: "Login e senha não correspondem",
+        });
+      }
+
+      const token = jwt.sign(
+        {
+          id: motorista.id,
+          apelido: motorista.apelido,
+        },
+        process.env.SECRET_KEY_TOKEN,
+        {
+          expiresIn: "30days",
+          // expiresIn: '1min'
         }
+      );
 
-        const token = jwt.sign({
-            id: motorista.id,
-            apelido: motorista.apelido
-        }, process.env.SECRET_KEY_TOKEN, {
-            expiresIn: '30days'
-            // expiresIn: '1min'
-        })
-
-        return res.status(HttpStatus.OK).json({
-            id: motorista.id,
-            token
-        })
+      return res.status(HttpStatus.OK).json({
+        id: motorista.id,
+        token,
+      });
     })
     .catch(err => {
-        console.log(err)
-        return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
-            mensagem: err
-        })
-    })
-})
+      console.log(err);
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        mensagem: err,
+      });
+    });
+});
 
-module.exports = router
+module.exports = router;
