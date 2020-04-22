32a6eea5c26ebb654ebf7975ad08a6d09b3f7828
Juaka <juakacc@gmail.com>
2020-04-22 13:38:30 -0300

    Documentando Login

--- src/controller/login.js
+++ src/controller/login.js
@@ -8,6 +8,42 @@ const Motorista = require("../models").motorista;
 
 const router = express.Router();
 
+/**
+ * @api {post} /login Autenticar-se na aplicação
+ * @apiName login
+ * @apiGroup Login
+ *
+ * @apiParam {String} apelido Apelido do motorista.
+ * @apiParam {String} senha   Senha do motorista.
+ *
+ * @apiParamExample {json} Request-Example:
+ *     {
+ *       "apelido": "joao123",
+ *       "senha": "adfasdf",
+ *     }
+ *
+ * @apiSuccess {Number} id    ID do motorista.
+ * @apiSuccess {String} token Token JWT de autenticação.
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *    HTTP/1.1 200 OK
+ *    {
+ *      "id": 1,
+ *      "token": "TOKEN_JWT"
+ *    }
+ *
+ * @apiErrorExample Error-Response:
+ *    HTTP/1.1 400 Bad Request
+ *    {
+ *      "mensagem": "Parâmetro inválido",
+ *    }
+ *
+ * @apiErrorExample Error-Response:
+ *    HTTP/1.1 400 Bad Request
+ *    {
+ *      "mensagem": "Login e senha não correspondem",
+ *    }
+ */
 router.post("/", (req, res, next) => {
   const {apelido, senha} = req.body;
 
@@ -45,11 +81,11 @@ router.post("/", (req, res, next) => {
           id: motorista.id,
           apelido: motorista.apelido,
         },
-        process.env.SECRET_KEY_TOKEN,
-        {
-          expiresIn: "30days",
-          // expiresIn: '1min'
-        }
+        process.env.SECRET_KEY_TOKEN
+        // {
+        //   expiresIn: "30days",
+        //   // expiresIn: '1min'
+        // }
       );
 
       return res.status(HttpStatus.OK).json({
