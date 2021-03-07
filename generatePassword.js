fc4447300ba9a77dae3de5a245f0e87c0f69e9d9
Joaquim <juakacc@gmail.com>
2021-03-07 10:09:21 -0300

    organize files

--- generatePassword.js
+++
@@ -1,4 +0,0 @@
-const bcrypt = require("bcryptjs");
-
-// console.log(bcrypt.hashSync("mikael", 10));
-console.log(bcrypt.compareSync("mikael", "$2a$10$deDO6Aw6dzEbKgMqMDC6uu2oKu4TL5V6K95td1Ju6vQmKCvaiN1hy"))
