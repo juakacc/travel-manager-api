2092128fbd5bc31ac440e9b96d47adcd32af6623
Joaquim <juakacc@gmail.com>
2021-05-13 08:10:20 -0300

    atualizando doc

---
+++ src/doc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "viagensPMO",
+  "version": "1.0.0",
+  "description": "Documentação básica da API Rest ViagensPMO",
+  "title": "ViagensPMO API Rest",
+  "url": "http://localhost:8888",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-05-13T11:08:22.493Z",
+    "url": "https://apidocjs.com",
+    "version": "0.26.0"
+  }
+});
